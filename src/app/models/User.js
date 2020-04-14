0144552c37ff4e26c66a987b8727eca97254976b
maxwneto <maxwneto@gmail.com>
2020-04-14 11:51:35 -0300

    criado model e controller de user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -9,13 +9,28 @@ class User extends Model {
         email: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
       }
     );
+
+    // criptografia da senha antes de inserir a mesma no banco
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 
+  /*
+  metodo para verificar se a senha do usuário está correta
+  se a senha for igual ele retorna true
+  e senão retorna falsa
+  */
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
